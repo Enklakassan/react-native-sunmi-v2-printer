e15819e2ed6928d1fb0650e35cc9ad7e3b67f21e
AdamAxenu <adam@axenu.com>
2025-03-31 12:08:40 +0200

    Add react method printQRCodeWithoutPromise

--- android/src/main/java/com/sunmi/v2/printer/SunmiV2PrinterModule.java
+++ android/src/main/java/com/sunmi/v2/printer/SunmiV2PrinterModule.java
@@ -960,6 +960,72 @@ public class SunmiV2PrinterModule extends ReactContextBaseJavaModule {
             }
         });
     }
+    
+        @ReactMethod
+    public void printQRCode(String data, int modulesize, int errorlevel, final Promise p) {
+        final IWoyouService ss = woyouService;
+        Log.i(TAG, "come: ss:" + ss);
+        final String d = data;
+        final int size = modulesize;
+        final int level = errorlevel;
+        ThreadPoolManager.getInstance().executeTask(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ss.printQRCode(d, size, level, new ICallback.Stub() {
+                        @Override
+                        public void onPrintResult(int par1, String par2) {
+                            Log.d(TAG, "ON PRINT RES: " + par1 + ", " + par2);
+                        }
+
+                        @Override
+                        public void onRunResult(boolean isSuccess) {
+                            if (isSuccess) {
+                                p.resolve(null);
+                            } else {
+                                p.reject("0", isSuccess + "");
+                            }
+                        }
+
+                        @Override
+                        public void onReturnString(String result) {
+                            p.resolve(result);
+                        }
+
+                        @Override
+                        public void onRaiseException(int code, String msg) {
+                            p.reject("" + code, msg);
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.i(TAG, "ERROR: " + e.getMessage());
+                    p.reject("" + 0, e.getMessage());
+                }
+            }
+        });
+    }
+    
+    @ReactMethod
+public void printQRCodeWithoutPromise(String data, int modulesize, int errorlevel) {
+    final IWoyouService ss = woyouService;
+    final String d = data;
+    final int size = modulesize;
+    final int level = errorlevel;
+
+    if (ss == null) {
+        Log.e(TAG, "printQRCodeWithoutPromise: WoyouService is not connected.");
+        return;
+    }
+
+    try {
+        ss.printQRCode(d, size, level, null);
+    } catch (Exception e) {
+        e.printStackTrace();
+        Log.e(TAG, "ERROR in printQRCodeWithoutPromise: " + e.getMessage());
+    }
+}
+
 
     /**
      * 打印文字，文字宽度满一行自动换行排版，不满一整行不打印除非强制换行
